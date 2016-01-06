c429fb5a4525d20aedf62e290b4b251290d5a759
Zachary Smith <billyzacsmith@gmail.com>
2016-01-06 08:01:27 -0700

    Add logout.

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req.session)
   res.render('index', { title: 'gdot' })
+})
+
+router.get('/logout', function(req, res, next) {
+  req.logout() // Removes req.user
+  res.locals.user = null // It seems we have to do this too. Weird.
+  console.log('======= user still here? ========', req.user)
+  res.render('index', { title: 'logged out' })
 })
 
 module.exports = router;
